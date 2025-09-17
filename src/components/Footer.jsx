2fb3dd013ab61faaa04b8fb61a242aff87ea0f4b
kunndnn <44980639+kunndnn@users.noreply.github.com>
2025-09-17 21:01:53 +0530

    footer component changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,8 @@ import { info } from "../data/info";
 
 const Footer = () => {
     return (
-        <footer className="bg-gradient-to-r from-green-200 to-green-100 shadow-inner mt-10">
-            <div className="max-w-5xl mx-auto flex items-center justify-between py-4">
+        <footer className="bg-gradient-to-r from-green-200 to-green-100 shadow-inner">
+            <div className="flex items-center justify-between p-5">
                 {/* Left side text */}
                 <span className="text-sm text-gray-700">
                     © {new Date().getFullYear()} <span className="font-semibold">Blogs</span>. Made with <span className="text-red-500">❤️</span>
@@ -15,7 +15,7 @@ const Footer = () => {
 
                     <span
                         onClick={() => window.open(info.socials.github, "_blank")}
-                        className="text-gray-700 hover:text-black transition transform hover:scale-110"
+                        className="text-gray-700 hover:text-black transition transform hover:scale-110 cursor-pointer"
                         title="Github"
                     >
                         <FaGithub size={22} />
@@ -23,7 +23,7 @@ const Footer = () => {
 
                     <span
                         onClick={() => window.open(info.socials.linkedin, "_blank")}
-                        className="text-gray-700 hover:text-blue-700 transition transform hover:scale-110"
+                        className="text-gray-700 hover:text-blue-700 transition transform hover:scale-110 cursor-pointer"
                         title="Linkedin"
                     >
                         <FaLinkedin size={22} />
@@ -31,7 +31,7 @@ const Footer = () => {
 
                     <span
                         onClick={() => window.open(info.socials.portfolio, "_blank")}
-                        className="text-gray-700 hover:text-orange-700 transition transform hover:scale-110"
+                        className="text-gray-700 hover:text-orange-700 transition transform hover:scale-110 cursor-pointer"
                         title="Portfoio"
                     >
                         <FaRocket size={22} />
@@ -39,9 +39,9 @@ const Footer = () => {
                     
                     <span
                         onClick={() => window.open(info.socials.code, "_blank")}
-                        className="text-gray-700 hover:text-orange-700 transition transform hover:scale-110"
+                        className="text-gray-700 hover:text-black transition transform hover:scale-110 cursor-pointer"
                         title="Source Code"
-                    >
+                    > 
                         <FaCode size={22} />
                     </span>
                 </div>
